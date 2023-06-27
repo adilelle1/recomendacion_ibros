37475c0c1949e67c674446c8e93e0b63ca816bb9
adilelle1 <106268116+adilelle1@users.noreply.github.com>
2023-06-27 11:58:08 -0300

    corrijo info de user id

--- deploy.py
+++ deploy.py
@@ -392,9 +392,9 @@ elif selected == 'Encontrá tu libro':
                 user_id = int(user_number)
                 primer_genero, segundo_genero, paginas_promedio = valores_frecuentes_usuario(user_id)
                 st.write(f'Número de usuario: {user_id}')
-                st.markdown(f'- Primer género favorito: ** {primer_genero} **')
-                st.markdown(f'- Segundo género favorito: ** {segundo_genero} **')
-                st.markdown(f'- Páginas promedio por libro: ** {paginas_promedio} **')
+                st.markdown(f'- Primer género favorito: **{primer_genero}**')
+                st.markdown(f'- Segundo género favorito: **{segundo_genero}**')
+                st.markdown(f'- Páginas promedio por libro: **{paginas_promedio}**')
             except ValueError:
                 st.warning('Por favor, ingresa un número de usuario válido.')
 
